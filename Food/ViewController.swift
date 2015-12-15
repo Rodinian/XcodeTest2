38792917121d3ff834cf345f5f2b7265300d80a1
Rodinian <chrodinian@gmail.com>
2015-12-15 15:27:02 +0100

    one new commit to test git

--- Food/ViewController.swift
+++ Food/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("this is a warning!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
